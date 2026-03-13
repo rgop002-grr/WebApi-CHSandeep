252dba8c9ebeb7f9b9376a1dc4088a11f37a769c
SandeepChavagani <chavaganisandeep7@gmail.com>
2026-03-13 18:30:56 +0530

    Implemented Jwt Token based authentication based on user role and implementing manual mapping Dtos

---
+++ WebApi-CHSandeep/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using BussinessLayer.IBusiness;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.ModelDto;
+
+namespace WebApi_CHSandeep.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthBussiness _authBussiness;
+
+        public AuthController(IAuthBussiness authService)
+        {
+            _authBussiness = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto loginDTO)
+        {
+            var token = _authBussiness.Login(loginDTO);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(new { Token = token });
+        }
+    }
+}
--- WebApi-CHSandeep/Program.cs
+++ WebApi-CHSandeep/Program.cs
@@ -1,15 +1,43 @@
 using BussinessLayer.Business;
 using BussinessLayer.IBusiness;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using RepositoryLayer.IRepository;
+using RepositoryLayer.JwtHelpers;
 using RepositoryLayer.Models;
 using RepositoryLayer.Repository;
 using Serilog;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 Log.Logger = new LoggerConfiguration()
     .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
+
+//jwt related
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey
+        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
 builder.Host.UseSerilog();
 
@@ -21,10 +49,41 @@ builder.Services.AddDbContext<SandeepContext>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Enter JWT Token like: Bearer {your token}",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddScoped<IProductBussiness, ProductBussiness>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+builder.Services.AddScoped<IAuthBussiness, AuthBussiness>();
+builder.Services.AddScoped<JwtTokenGenerator, JwtTokenGenerator>();
 
 var app = builder.Build();
 
@@ -37,6 +96,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
